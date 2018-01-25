9141f223d074c0a97435b751ae81ece81f287dec
Gustavus Shaw II <gshaw1997@gmail.com>
2018-01-25 17:28:43 -0500

    Removed dist from repo

--- dist/interactors/interactors.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SuggestionInteractor_1 = require("./SuggestionInteractor");
-exports.SuggestionInteractor = SuggestionInteractor_1.SuggestionInteractor;
