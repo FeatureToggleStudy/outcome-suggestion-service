57d3d8e3b7e7758d9d8133880b027ad84f571647
KolbeML <kleona3@students.towson.edu>
2018-04-17 12:53:43 -0400

    Update card template Improve card template, remove redundant variables in FDF form structure

--- src/business-cards/business-cards.ts
+++ src/business-cards/business-cards.ts
@@ -16,40 +16,13 @@ export async function fillPdf(
   const url: string = 'clark.center/' + user_name;
   const sourcePDF: string = path.join(
     __dirname,
-    '../businesscardformempty.pdf',
+    '../businesscardformemptynew.pdf',
   );
 
   const data = {
-    Name2: name,
-    Org2: org,
     Org1: org,
-    Name3: name,
-    Org3: org,
-    Name4: name,
-    Org4: org,
-    Name5: name,
-    Org5: org,
-    Name6: name,
-    Org6: org,
-    Name7: name,
-    Org7: org,
-    Name8: name,
-    Org8: org,
-    Name9: name,
-    Org9: org,
-    Name10: name,
-    Org10: org,
     Name1: name,
     URL1: url,
-    URL2: url,
-    URL9: url,
-    URL10: url,
-    URL3: url,
-    URL4: url,
-    URL5: url,
-    URL6: url,
-    URL7: url,
-    URL8: url,
   };
 
   try {
