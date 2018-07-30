3ae9676f576d5fa9ed3a41cbb4231ed86d9b4975
Sean Donnelly <sdonne5@students.towson.edu>
2018-07-30 12:58:59 -0400

    :whale: Optimize Filesystem Usage in Containers

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 const ts = require('gulp-typescript');
+const watch = require('gulp-watch');
 const JSON_FILES = ['package.json', 'src/*.json', 'src/**/*.json'];
 
 const BUSINESS_CARD_TEMPLATE = ['src/business-cards/businesscardformempty.pdf'];
@@ -14,18 +15,24 @@ gulp.task('tsc', () => {
 });
 
 gulp.task('watch', ['tsc'], () => {
-  gulp.watch('src/**/*.ts', ['tsc']);
+  watch('src/**/*.ts', { usePolling: true, interval: 2000 }, function(file) {
+    gulp.start('tsc')
+  });
 });
 
 gulp.task('assets', function() {
   gulp.src([...JSON_FILES, ...BUSINESS_CARD_TEMPLATE]).pipe(gulp.dest('dist'));
 });
 
-gulp.task('start', ['watch', 'assets'], function() {
+gulp.task('start', ['watch'], function() {
+  inDocker = process.env.IN_DOCKER == 'true';
+  
   nodemon({
     script: 'dist/app.js',
-    ext: 'js',
+    ext: 'js html',
     watch: ['./dist'],
+    legacyWatch: inDocker,
+    delay: 5
   });
 });
 
